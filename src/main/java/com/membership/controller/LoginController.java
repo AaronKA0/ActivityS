9deb03665c2e5fc71dd1735f3034fae13249696a
Aaron Kao <aaron38123@gmail.com>
2024-02-03 19:44:49 +0800

    update feedback

--- src/main/java/com/membership/controller/LoginController.java
+++ src/main/java/com/membership/controller/LoginController.java
@@ -76,17 +76,12 @@ public class LoginController {
 //	        System.out.print("line 65 :" + "memId:" + memId +" "+"memAcc:" + memAcc +" " + "memPwd:" + memPwd);
 
 
-//			// +++++++++++++++ 將會員個人訊息存入 session +++++++++++++++
-//			List<NotifyVO> notifies = notifySvc.findByMemId(memId);
-//			request.getSession().setAttribute("notifies", notifies);
-//			// +++++++++++++++ 將會員個人訊息存入 session +++++++++++++++
-
 			// 每次登入就更新一次登入時間
 			membershipSvc.updateMemLoginTime(memAcc);
 
 
-//			return "redirect:/Zuo-Huo";
-			return "redirect:/member";
+			return "redirect:/Zuo-Huo";
+//			return "redirect:/member";
 
 
 		} else {
